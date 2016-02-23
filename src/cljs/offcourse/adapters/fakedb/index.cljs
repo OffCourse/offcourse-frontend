51403a262f09d5616eb8b1ff7646841c1701095b
Jan Hein Hoogstad <yeehaa@codingthehumanities.com>
2016-02-23 15:30:16 +0100

    fix merge conflicts

--- src/cljs/offcourse/adapters/fakedb/index.cljs
+++ src/cljs/offcourse/adapters/fakedb/index.cljs
@@ -10,14 +10,7 @@
   (start [db] (assoc db :connection "test/test.com"))
   (stop  [db] (dissoc db :connection))
   Queryable
-  (-check   [db query] #_(check db query))
-<<<<<<< HEAD
   (-fetch   [db query] (fetch db query))
-  (-refresh [db query] #_(refresh db query))
-=======
-  (fetch   [db query] (fetch db query))
-  (refresh [db query] #_(refresh db query))
->>>>>>> b0f61bca632fb81094e7cfb4a96acd99ee387a6b
   Validatable
   (valid? [db] #_(valid? db))
   Responsive
